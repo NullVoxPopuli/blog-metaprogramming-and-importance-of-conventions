8be07e025810f100ff7b88ffd94f961f745a31b2
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-04-15 09:28:09 -0400

    Update 01-run-intro.rb

--- 01-intro-to-metaprogramming/01-run-intro.rb
+++ 01-intro-to-metaprogramming/01-run-intro.rb
@@ -6,16 +6,16 @@ require './tesla'
 
 puts "For the DeLorean".yellow
 best_car = DeLorean.new
-assert_false best_car.has_moon_roof?
-assert_true best_car.has_flux_capacitor?
-assert_true best_car.has_gull_wing_doors?
-assert_true best_car.has_windows?
+assert_false best_car.has_moon_roof? # => false
+assert_true best_car.has_flux_capacitor? # => true
+assert_true best_car.has_gull_wing_doors? # => true
+assert_true best_car.has_windows? # => true
 
 puts "\n"
 puts "For the Tesla".yellow
 expensive_car = Tesla.new
-assert_true expensive_car.has_moon_roof?
-assert_false expensive_car.has_flux_capacitor?
-assert_false expensive_car.has_gull_wing_doors?
-assert_true expensive_car.has_windows?
-assert_true expensive_car.has_turbo?
+assert_true expensive_car.has_moon_roof? # => true
+assert_false expensive_car.has_flux_capacitor? # => false
+assert_false expensive_car.has_gull_wing_doors? # => false
+assert_true expensive_car.has_windows? # => true
+assert_true expensive_car.has_turbo? # => true
